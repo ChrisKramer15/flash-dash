8cdb956b1e161807b57cde4f696f1388e3c32c7f
Chris Kramer <ChrisKramer15@outlook.com>
2021-04-13 21:24:54 -0500

    able to console log inputs

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-let minimum = document.getElementById('#minOffer');
-let distance = document.getElementById('#totDistance');
-let grocery = document.getElementById('#groceryCheck');
-let offer = document.getElementById('#offerAmt');
-
-let parsedMinimum = parseInt(minimum);
-let parsedDistance = parseInt(distance);
-let parsedOffer = parseInt(offer);
-
 function analyze() {
-    console.log(typeof(offer));
-    if(parsedOffer >= parsedMinimum) {
-        console.log('pass');
-    }
-    else {
-        console.log('fail');
-    }
+let minimum = document.querySelector("#minOffer");
+let offer = document.querySelector("#offerAmt");
+let distance = document.querySelector("#totDistance");
+let grocery = document.querySelector("#groceryCheck:checked");
+
+minimum = parseInt(minimum.value);
+offer = parseInt(offer.value);
+distance = parseInt(distance.value)
+if (grocery === null){
+    console.log('false');
+}
+else{
+    console.log('true');
+}
 }
