40039421a6951b97a49c48a3928eb70e3abc26f4
Chris Kramer <ChrisKramer15@outlook.com>
2023-08-04 22:06:51 -0500

    updated with major updates

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
-let offerInput = localStorage.getItem("offerSave");
-let minimum = document.querySelector("#minOffer")
-minimum.value = offerInput;
-
 let restInput = localStorage.getItem("restSave");
-let rest = document.querySelector("#numOrders");
+let rest = document.querySelector("#numPlaces");
 rest.value = restInput;
-
-let distInput = localStorage.getItem("distSave");
-// distance.value = distInput;
+let orderInput = localStorage.getItem("orderSave");
+let ord = document.querySelector("#numOrders");
+ord.value = orderInput;
 
 function analyze() {
-let radios = document.getElementsByName('rateRadio');
-let setRate = 0;
-for (let i = 0, length = radios.length; i < length; i++) {
-  if (radios[i].checked) {
-      setRate = parseFloat(radios[i].value);
+let radios = document.querySelector('#customRange1');
+radios = parseFloat(radios.value);
 
-    // only one radio can be logically checked, don't check the rest
-    break;
-  }
-}
-let minimum = document.querySelector("#minOffer");
-localStorage.setItem("offerSave", minimum.value)
 let offer = document.querySelector("#offerAmt");
 let distance = document.querySelector("#totDistance");
 let grocery = document.querySelector("#groceryCheck");
@@ -29,62 +16,40 @@ let passDisplay = document.querySelector("#pass");
 let failDisplay = document.querySelector("#fail");
 let pphDisplay = document.querySelector("#pph");
 let numOrders = document.querySelector("#numOrders");
-localStorage.setItem("restSave", numOrders.value);
+let numPlaces = document.querySelector("#numPlaces")
+localStorage.setItem("restSave", numPlaces.value);
+localStorage.setItem("orderSave", numOrders.value);
 let statusContain = document.querySelector("#primaryContainer");
 
-minimum = parseFloat(minimum.value);
 offer = parseFloat(offer.value);
 distance = parseFloat(distance.value);
 numOrders = parseInt(numOrders.value);
+numPlaces = parseInt(numPlaces.value);
 
 //amount of allowed minutes to complete order
 let maxMinutes = 0;
-console.log("max minutes: " + maxMinutes)
-maxMinutes = 60 / (setRate / offer) ;
+maxMinutes = 60 / (radios / offer) ;
 //allowed minutes minus restaurant wait time
-console.log("max minutes: " + maxMinutes)
-let restaurant = 5;
+let restaurant = 5 * numOrders;
 if (grocery.checked) {
   restaurant = 10;
 }
-restaurant *= numOrders;
+restaurant *= numPlaces;
 let timeRemaining = maxMinutes - restaurant;
-console.log(`time remaining: ${timeRemaining}`)
 timeRemaining = timeRemaining - (distance * 4);
-console.log(`time remaining: ${timeRemaining}`)
+let perHour = (offer/(restaurant + (distance * 4)) * 60).toFixed(2)
+pphDisplay.textContent = `$${perHour} per hour`
 
-minimum *= numOrders;
-
-
-//calculate approx. money received per minute
-// let rate = offer / approxTime;
-
-//if grocery order, minimum required is doubled to account for wait times/effort required
-if (grocery.checked){
-    minimum *= 2;
-}
-
-
-//condition statements -- verifies minimum amount and rate have been achieved
-if(offer >= minimum) {
-  pphDisplay.textContent = `$${(offer/(restaurant + (distance * 4)) * 60).toFixed(2)} per hour`
-    if(timeRemaining >= 0){
-        
-        passDisplay.style.display = "block";
-        failDisplay.style.display = "none";
-        statusContain.style.backgroundColor = "green";
-    }else {
-        failDisplay.style.display = "block";
-        passDisplay.style.display = "none";
-        statusContain.style.backgroundColor = "red";
-    }
-}else {
-    failDisplay.style.display = "block";
-    passDisplay.style.display = "none";
-    statusContain.style.backgroundColor = "red";
-}
-
-
+//condition statements -- verifies rate has been achieved
+if(perHour >= radios){
+  passDisplay.style.display = "block";
+  failDisplay.style.display = "none";
+  statusContain.style.backgroundColor = "green";
+  }else {
+      failDisplay.style.display = "block";
+      passDisplay.style.display = "none";
+      statusContain.style.backgroundColor = "red";
+  }
 }
 
 
